9597648ede7eceaaa6976ed1bfbab8883ae3f46e
LtMilligan <gioele.miscia@gmail.com>
2024-10-20 01:02:31 +0200

    [UPD] Add category seeder

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -2,6 +2,8 @@
 namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use Illuminate\Support\Str;
 class CategorySeeder extends Seeder
 {
     /**
@@ -11,6 +13,12 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        $categories  = ['Frontend', 'Backend', 'Database', 'Design', 'Fullstack', 'Programming'];
+        foreach ($categories as $cat) {
+            $category= new Category();
+            $category->name = $cat;
+            $category->slug = Str::slug($cat, '-');
+            $category->save();
+        }
     }
 }
